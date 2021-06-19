760f4e8ace9e079b13291d445000bd3e932f4dbf
COCONUT <64309783+rtq9731@users.noreply.github.com>
2021-06-19 15:51:12 +0900

    집 -> 학교

---
+++ 13rd/Tree_Algorithm/ThirdTry/Node.cpp
@@ -0,0 +1,42 @@
+#include "Node.h"    
+
+Node::Node(int data)
+{
+    this->data = data;
+}
+
+int Node::getData()
+{
+    return this->data;
+}
+
+void Node::setData(int data)
+{
+    this->data = data;
+}
+
+void Node::linkToLeft(Node* linkNode)
+{
+    if (this->left != NULL)
+        delete this->left;
+
+    this->left = linkNode;
+}
+
+void Node::linkToRight(Node* linkNode)
+{
+    if (this->right != NULL)
+        delete this->right;
+
+    this->right = linkNode;
+}
+
+Node* Node::getLeftNode()
+{
+    return this->left;
+}
+
+Node* Node::getRightNode()
+{
+    return this->right;
+}
--- 13rd/Tree_Algorithm/ThirdTry/main.cpp
+++ 13rd/Tree_Algorithm/ThirdTry/main.cpp
@@ -1,6 +1,45 @@
-#include <iostream>
+#include <string>
+#include <vector>
+#include "Node.h"	
+
+using namespace std;
+
+vector<string> stringVector;
+vector<Node> nodeVector;
+
+int nodeCount = 0;
+
+string preoderStirng;
+string postorderString;
+
+Node* findNodeByData(int data)
+{
+	Node* ptr_Temp = nullptr;
+	return ptr_Temp;
+}
 
 int main()
 {
+	/*
+	cout << "전체 노드는 몇개입니까? : ";
+	cin >> nodeCount;
 
+	cout << "전위 순회를 한 노드 순서를 입력해주세요 : ";
+	for (int i = 0; i < nodeCount; i++)
+	{
+		cin >> preoderStirng[i];
+	}
+
+	cout << "후위 순회를 한 노드 순서를 입력해주세요 : ";
+	for (int i = 0; i < nodeCount; i++)
+	{
+		cin >> postorderString[i];
+	}
+	*/
+
+	Node n = Node(10);
+	Node* ptr_Node = findNodeByData(1);
+	ptr_Node = &n;
+	
+	cout << ptr_Node->data;
 }
